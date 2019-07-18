ad66850439ab79025bfb4d653e2df454178c24bd
tmg2017 <519208032@qq.com>
2019-07-18 10:09:18 +0800

    update dep

--- typings/app/controller/index.d.ts
+++ typings/app/controller/index.d.ts
@@ -1,17 +1,17 @@
 // This file is created by egg-ts-helper@1.25.3
 // Do not modify this file!!!!!!!!!
 
-import 'egg';
-import ExportArticle from '../../../app/controller/article';
-import ExportAuth from '../../../app/controller/auth';
-import ExportComment from '../../../app/controller/comment';
-import ExportHome from '../../../app/controller/home';
+import 'egg'
+import ExportArticle from '../../../app/controller/article'
+import ExportAuth from '../../../app/controller/auth'
+import ExportComment from '../../../app/controller/comment'
+import ExportHome from '../../../app/controller/home'
 
 declare module 'egg' {
   interface IController {
-    article: ExportArticle;
-    auth: ExportAuth;
-    comment: ExportComment;
-    home: ExportHome;
+    article: ExportArticle
+    auth: ExportAuth
+    comment: ExportComment
+    home: ExportHome
   }
 }
